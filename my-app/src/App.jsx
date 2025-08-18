3cf1fc521b028c0010c1eb662627828ff223350f
Sahil Chaudhary <sahilchaudhary975832@gmail.com>
2025-08-18 22:50:50 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'
-import Section3 from './components/section3'
+import Section3 from './components/Section3'
 import Section4 from './components/Section4'
 import Section5 from './components/Section5'
 import Section6 from './components/Section6'
