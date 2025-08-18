7d574d9d01a1dcf3eede9fcc4f9de088ef492b0c
Sahil Chaudhary <sahilchaudhary975832@gmail.com>
2025-08-18 22:54:06 +0530

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/ArivanaAi",
+  base: './',   // ✅ use relative paths for Netlify
 })
-
